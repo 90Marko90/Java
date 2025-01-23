faee4c2ae485867aac8fec03fc7d4d34e2ef648d
marek krncok <krncok36@gmail.com>
2025-01-23 18:30:05 +0100

    init commit

---
+++ src/main/java/com/example/tenisuj/TenisujApplication.java
@@ -0,0 +1,13 @@
+package com.example.tenisuj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisujApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisujApplication.class, args);
+	}
+
+}
