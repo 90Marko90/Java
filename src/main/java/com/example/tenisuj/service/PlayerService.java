b8841d4cf534b0c61ec0e535734cf6251e577503
marek krncok <krncok36@gmail.com>
2025-01-26 20:07:44 +0100

    player implementation

---
+++ src/main/java/com/example/tenisuj/service/PlayerService.java
@@ -0,0 +1,23 @@
+package com.example.tenisuj.service;
+
+import com.example.tenisuj.model.Player;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PlayerService {
+    Player addPlayer(@NonNull String firstName,
+                     @NonNull String lastName,
+                     String email,
+                     String gender,
+                     LocalDate birthday,
+                     Boolean leagueStatus,
+                     String hand,
+                     int rating,
+                     LocalDate registrationDate);
+    List<Player> getAllPlayers();
+    Player getPlayerById(@NonNull String id);
+    void deletePlayer(@NonNull String id);
+
+}
