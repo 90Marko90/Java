e7bae3d07c9c69e793170ec52ec3272a2ce08614
GitMatus <matus.machovic@outlook.com>
2025-01-28 21:13:20 +0100

    test

--- src/main/java/com/example/tenisuj/mytestpackage/testclass.java
+++
@@ -1,4 +0,0 @@
-package com.example.tenisuj.mytestpackage;
-
-public class testclass {
-}
--- src/main/java/com/example/tenisuj/service/MatchServiceBean.java
+++ src/main/java/com/example/tenisuj/service/MatchServiceBean.java
@@ -1,13 +1,17 @@
 package com.example.tenisuj.service;
 
+import com.example.tenisuj.model.League;
 import com.example.tenisuj.model.Match;
 import com.example.tenisuj.model.Player;
+import com.example.tenisuj.model.User;
 import com.example.tenisuj.repository.MatchRepository;
 import com.example.tenisuj.repository.PlayerRepository;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.UUID;
@@ -34,13 +38,17 @@ public class MatchServiceBean implements MatchService {
         Player player2 = playerRepository.findById(player2Id)
                 .orElseThrow(() -> new RuntimeException("Player not found"));
 
-        Match match = new Match(UUID.randomUUID().toString(),player1,player2,null);
-//        Match match = new Match();
-//        match.setId(UUID.randomUUID().toString());
-//        match.setPlayer(player1);
+        Match match = new Match(UUID.randomUUID().toString(),player1,player2,null,null,null,null,null,null,null,null,null,null,null);
 
         matchRepository.save(match);
         log.info("Match added");
+        return match;
+    }
+
+    @Override
+    public Match getMatch(String matchId) {
+        Match match = matchRepository.findById(matchId)
+                .orElseThrow(() -> new RuntimeException("Match not found"));
         return match;
     }
 
@@ -56,5 +64,34 @@ public class MatchServiceBean implements MatchService {
         }
         matchRepository.deleteById(matchId);
         log.info("Match deleted");
+    }
+
+    @Override
+    public void addResult(String matchId,
+                           Integer player1_set1,
+                           Integer player2_set1,
+                           Integer player1_set2,
+                           Integer player2_set2,
+                           Integer player1_set3,
+                           Integer player2_set3,
+                           Integer player1_set4,
+                           Integer player2_set4,
+                           Integer player1_set5,
+                           Integer player2_set5,
+                           String playerId) {
+
+        var match = matchRepository
+                .findById(matchId)
+                .orElseThrow(() -> new UsernameNotFoundException("Match not found"));
+
+        Player scratchnigPlayer = playerRepository.findById(playerId)
+                .orElseThrow(() -> new RuntimeException("Player not found"));
+        // need to add validation if scratchingPlayer is Player1 or Player2
+
+        Match updated = new Match(matchId,matchRepository.findById(matchId).get().getPlayer1(),matchRepository.findById(matchId).get().getPlayer1(),player1_set1,player2_set1,player1_set2, player2_set2, player1_set3, player2_set3, player1_set4, player2_set4, player1_set5, player2_set5, scratchnigPlayer);
+
+        matchRepository.save(updated);
+        log.info("Match result updated: {}", updated.getId());
+
     }
 }
